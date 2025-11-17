e446b0fd0339cbd3d1b2d8de8ab6474044658298
Raka <rakakamolan@gmail.com>
2025-11-17 13:19:05 +0800

    fixxxx

--- clientside/lib/firebase.ts
+++ clientside/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyD2sDArKX1-hESo3afXs8BdECi1yDk_ypE",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "reservationweb-4b61a.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "reservationweb-4b61a",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "reservationweb-4b61a.appspot.com",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "reservationweb-4b61a.firebasestorage.app",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "384156096887",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:384156096887:web:d537ee74d242d8136ee972",
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-RC36JZD21V",
