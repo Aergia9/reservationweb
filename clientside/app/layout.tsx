6649c7b093c685cc31147f674b655f0b45eee3af
Raka <rakakamolan@gmail.com>
2025-12-31 10:31:56 +0800

    tab icon update

--- clientside/app/layout.tsx
+++ clientside/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Claro Event Reservation",
+  icons: {
+    icon: "/clarologo.png",
+  },
 };
 
 export default function RootLayout({
